abf7ddcc646cb37c1e01105ed2a86f959fa1fd93
Gabès Jean <naparuba@gmail.com>
2016-09-19 17:08:15 +0200

    Fix: dns return format

--- kunai/dnsquery.py
+++ kunai/dnsquery.py
@@ -1,6 +1,8 @@
 import re
 import socket
 
+from kunai.log import logger
+
 pattern = r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)([ (\[]?(\.|dot)[ )\]]?(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})"
 ipv4pattern = re.compile(pattern)
 
@@ -29,23 +31,26 @@ class DNSQuery:
     
     # We look in the nodes for the good tag
     def lookup_for_nodes(self, nodes, dom):
-        print "DNS LOOKING ", self.domain, "inside domain", dom
+        logger.debug('Querying %s for managed domaine: %s' % (dom, self.domain), part='dns')
         if not self.domain.endswith(dom):
+            logger.debug('Domain %s is not matching managed domain: %s' % (dom, self.domain), part='dns')
             return []
         search = self.domain[:-len(dom)]
         # split into sname.service.datacenter
-        print "DNS lookup for search", search
+        logger.debug("Lookup for search %s" % search, part='dns')
         elts = search.split('.', 2)
         if len(elts) != 3:
-            print "DNS bad query", search
+            logger.debug('Bad query, not 3 dots in %s' % search, part='dns')
             return []
         dc = elts[2]
         _type = elts[1]
         tag = elts[0]
+        logger.debug('Looking in %s nodes' % len(nodes), part='dns')
         r = []
         for n in nodes.values():
             # skip non alive nodes
             if n['state'] != 'alive':
+                logger.debug('Skipping node %s, state=%s != alive' % (n['name'], n['state']))
                 continue
             if tag in n['tags']:
                 services = n.get('services', {})
@@ -53,37 +58,53 @@ class DNSQuery:
                 if tag in services:
                     service = services[tag]
                     state_id = service.get('state_id')
-                print "DNS state_id", state_id
-                if state_id == 0:
-                    addr = n['addr']
-                    # If already an ip, add it
-                    if ipv4pattern.match(addr):
+                logger.debug('current state id : %s' % state_id, part='dns')
+                # Skip bad nodes
+                if state_id != 0:
+                    logger.debug('Skipping node %s' % n['name'])
+                    continue
+
+                addr = n['addr']
+                # If already an ip, add it
+                if ipv4pattern.match(addr):
+                    r.append(addr)
+                else:  # else try to resolv it first
+                    try:
+                        addr = socket.gethostbyname(addr)
                         r.append(addr)
-                    else:  # else try to resolv it first
-                        try:
-                            addr = socket.gethostbyname(addr)
-                            r.append(addr)
-                        except socket.gaierror:  # not found
-                            print 'DNS cannot find the hotname ip', addr
-                            # skip this node
+                    except socket.gaierror:  # not found
+                        print 'DNS cannot find the hotname ip', addr
+                        # skip this node
         
-        print "DNS R:", r
+        logger.debug('DNS return %s' % r)
         return r
+    
+    # Get origianl question from DATA, and only this, so stip to NAME+0001(typeA)+0001(IN) and drop what is after
+    def __get_origianl_domain_name_question(self, data):
+        # first find the end of the string, with \x00
+        end_string_idx = data.find('\x00')
+        if end_string_idx == -1:
+            # ok bad query, fuck off
+            return data
+        # so give 2bytes after the end
+        return data[:end_string_idx+5]  # 5=1 for \00 and 2+2 for typeA+IN
     
     
     def response(self, r):
         packet = ''
-        print "DNS DOM", self.domain
+        #r = ['192.168.56.103', '192.168.56.104' , '192.168.56.105']
         nb = len(r)
         if self.domain:
             packet += self.data[:2] + "\x81\x80"
-            packet += self.data[4:6] + self._get_size_hex(nb) + '\x00\x00\x00\x00'  # Questions and Answers Counts
-            packet += self.data[12:]  # Original Domain Name Question
+            packet += self.data[4:6] + self._get_size_hex(nb)  + '\x00\x00\x00\x00'  # Questions and Answers Counts
+            packet += self.__get_origianl_domain_name_question(self.data[12:])  # Original Domain Name Question
             
             for ip in r:
                 packet += '\xc0\x0c'  # Pointer to domain name
                 packet += '\x00\x01\x00\x01\x00\x00\x00\x3c\x00\x04'  # Response type, ttl and resource data length -> 4 bytes
                 packet += str.join('', map(lambda x: chr(int(x)), ip.split('.')))  # 4bytes of IP
-        
+
+
+
         print "DNS RETURNing", len(packet), len(r)
         return packet
