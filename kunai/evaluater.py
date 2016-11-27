e1c5fd4b36dd732a26fe14cd8d92aee804eeeee3
Gabès Jean <naparuba@gmail.com>
2016-11-27 14:44:01 +0100

    Enh: function expression documentation.

--- kunai/evaluater.py
+++ kunai/evaluater.py
@@ -63,21 +63,31 @@ def export(f):
 
 @export
 def file_exists(path):
-    """file_exists(path) -> return True if a path exist on the system, False otherwise.
-    * path: (string) path to check.
+    """**file_exists(path)** -> return True if a path exist on the system, False otherwise.
+
+ * path: (string) path to check.
     
+<code>
     Example: file_exists('/etc/mongodb.conf')
-    """
+    Returns: True
+</code>
+    
+"""
     return os.path.exists(path)
 
 
 @export
 def ip_is_in_range(ip, range):
-    """ip_is_in_range(ip, range) -> return True if the ip is in the ip range, False otherwise.
-    * ip:     (string) ip (v4 or v6) to check
-    * range:  (string) ip range that the ip must be in
+    """**ip_is_in_range(ip, range)** -> return True if the ip is in the ip range, False otherwise.
     
+ * ip:     (string) ip (v4 or v6) to check
+ * range:  (string) ip range that the ip must be in
+    
+    
+<code>
     Example:  ip_is_in_range('172.16.0.30', '172.16.0.0/24')
+    Returns: True
+</code>
     """
     
     ip_range = IP(range)
@@ -86,12 +96,16 @@ def ip_is_in_range(ip, range):
 
 @export
 def grep_file(string, path, regexp=False):
-    """file_exists(path) -> return True if a string or a regexp match the content of a file, False otherwise.
-    * string: (string)  string (or regexp expression) to check
-    * path: (string) path of the file to look inside.
-    * regexp: (boolean) is the string a regexp or not.
+    """**file_exists(path)** -> return True if a string or a regexp match the content of a file, False otherwise.
     
+ * string: (string)  string (or regexp expression) to check
+ * path: (string) path of the file to look inside.
+ * regexp: (boolean) is the string a regexp or not.
+    
+<code>
     Example: grep_file('centos', '/etc/redhat-release')
+    Returns: True
+</code>
     """
     s = string
     p = path
@@ -133,10 +147,14 @@ yumbase = None
 
 @export
 def has_package(package):
-    """has_package(package) -> return True if the package is installed on the system, False otherwise.
-    * package: (string) name of the package to check for.
+    """**has_package(package)** -> return True if the package is installed on the system, False otherwise.
     
+ * package: (string) name of the package to check for.
+
+<code>
     Example: has_package('postfix')
+    Returns: False
+</code>
     """
     global deb_cache, deb_cache_update_time, dpkg_cache_last_modification_epoch
     global yumbase
@@ -173,12 +191,17 @@ def has_package(package):
 
 @export
 def check_tcp(host, port, timeout=10):
-    """check_tcp(host, port, timeout=10) -> return True if the TCP connection can be established, False otherwise.
-    * host: (string) ip/fqdn of the host to connect to.
-    * port: (integer) TCP port to connect to
-    * timeout [optionnal] (integer) timeout to use for the connection test.
+    """**check_tcp(host, port, timeout=10)** -> return True if the TCP connection can be established, False otherwise.
+    
+ * host: (string) ip/fqdn of the host to connect to.
+ * port: (integer) TCP port to connect to
+ * timeout [optionnal] (integer) timeout to use for the connection test.
 
-    Example: check_tcp('www.google.com', 80)
+<code>
+ Example: check_tcp('www.google.com', 80)
+ Returns: True
+</code>
+
     """
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -194,11 +217,12 @@ def check_tcp(host, port, timeout=10):
 
 @export
 def get_os():
-    """get_os() -> return a string about the os.
+    """**get_os()** -> return a string about the os.
 
+<code>
     Example: get_os()
-    
-    Returns:  linux
+    Returns:  'linux'
+</code>
     """
     import platform
     return platform.system().lower()
@@ -206,10 +230,15 @@ def get_os():
 
 @export
 def have_tag(tag):
-    """have_tag(tag) -> return True if the node have the tag, False otherwise.
-    * tag: (string) tag to check.
+    """**have_tag(tag)** -> return True if the node have the tag, False otherwise.
+    
+ * tag: (string) tag to check.
 
+
+<code>
     Example: have_tag('linux')
+    Returns: True
+</code>
     """
     return gossiper.have_tag(tag)
 
