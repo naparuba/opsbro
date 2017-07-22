e002c0e066a3ae2b3de46e517df2b4353b8f1993
Jean Gabès <naparuba@gmail.com>
2017-07-22 13:59:27 +0200

    Fix: (ubuntu 14.04) Some old requests libs do not have rq.packages.urllib3 and direclty map them to rq.exceptions.RequestException Fix #6

--- kunai/httpclient.py
+++ kunai/httpclient.py
@@ -1,3 +1,8 @@
 import requests as rq
 
-HTTP_EXCEPTIONS = (rq.exceptions.RequestException, rq.packages.urllib3.exceptions.HTTPError)
+# Some old requests libs do not have rq.packages.urllib3 and direclty map them to rq.exceptions.RequestException
+# like in ubuntu 14.04 version
+if hasattr(rq, 'packages'):
+    HTTP_EXCEPTIONS = (rq.exceptions.RequestException, rq.packages.urllib3.exceptions.HTTPError)
+else:
+    HTTP_EXCEPTIONS = (rq.exceptions.RequestException, )
