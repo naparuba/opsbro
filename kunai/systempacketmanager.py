c82dd4314bd55ddb420b7110e02c4cdde535c6ad
Jean Gabès <naparuba@gmail.com>
2017-07-22 14:04:15 +0200

    Add: system package is now able to install package on apt & yum system. Also give distro name & version. Fix #9

--- kunai/systempacketmanager.py
+++ kunai/systempacketmanager.py
@@ -1,8 +1,11 @@
 import time
 import os
 import platform
+import subprocess
+import threading
 
 from kunai.log import LoggerFactory
+
 # Global logger for this part
 logger = LoggerFactory.create_logger('system-packages')
 
@@ -24,6 +27,10 @@ class DummyBackend(object):
     
     def has_package(self, package):
         return False
+    
+    
+    def install_package(self, package):
+        raise NotImplemented()
 
 
 class AptBackend(object):
@@ -35,8 +42,23 @@ class AptBackend(object):
         self.dpkg_cache_last_modification_epoch = 0.0
         # query cache, invalidate as soon as the apt cache is gone too
         self.has_cache = {}
+
+    # Maybe the apt module is not present, if so, fix it
+    def assert_apt(self):
+        global apt
+        if apt is None:
+            try:
+                self.install_package('python-apt')
+                import apt
+            except Exception, exp:
+                logger.error('Cannot install APT python lib: %s' % exp)
+                return
+    
     
     def has_package(self, package):
+        self.assert_apt()
+        if apt is None:
+            return False
         t0 = time.time()
         if not self.deb_cache:
             self.deb_cache = apt.Cache()
@@ -63,45 +85,139 @@ class AptBackend(object):
             b = (package in self.deb_cache and self.deb_cache[package].is_installed)
             logger.debug('APT: cache miss. time to query apt for package %s: %.3f' % (package, time.time() - t0))
         return b
+    
+    
+    # apt-get -q --yes --no-install-recommends install XXXXX
+    @staticmethod
+    def install_package(package):
+        logger.debug('APT :: installing package: %s' % package)
+        p = subprocess.Popen(['apt-get', 'update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('APT (apt-get update):: stdout/stderr: %s/%s' % (stdout, stderr))
+        if p.returncode != 0:
+            raise Exception('APT: apt-get update did not succeed (%s), exiting from package installation (%s)' % (stdout + stderr, package))
+        p = subprocess.Popen(['apt-get', '-q', '--yes', '--no-install-recommends', 'install', r'%s' % package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('APT (apt-get install) (%s):: stdout/stderr: %s/%s' % (package, stdout, stderr))
+        if p.returncode != 0:
+            raise Exception('APT: apt-get install did not succeed (%s), exiting from package installation (%s)' % (stdout + stderr, package))
+        return
 
 
 class YumBackend(object):
     def __init__(self):
         self.yumbase = None
+        self.yumbase_lock = threading.RLock()
     
     
     def has_package(self, package):
-        if not self.yumbase:
-            self.yumbase = yum.YumBase()
-            self.yumbase.conf.cache = 1
+        # Yum conf seem to be global and so cannot set it in 2 threads at the same time
+        with self.yumbase_lock:
+            if not self.yumbase:
+                self.yumbase = yum.YumBase()
+                self.yumbase.conf.cache = 1
         return package in (pkg.name for pkg in self.yumbase.rpmdb.returnPackages())
+    
+    
+    # yum  --nogpgcheck  -y  --rpmverbosity=error  --errorlevel=1  --color=auto  install  XXXXX
+    @staticmethod
+    def install_package(package):
+        logger.debug('YUM :: installing package: %s' % package)
+        p = subprocess.Popen(['yum', '--nogpgcheck', '-y', '--rpmverbosity=error', '--errorlevel=1', '--color=auto', 'install', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('YUM (%s):: stdout: %s' % (package, stdout))
+        logger.debug('YUM (%s):: stderr: %s' % (package, stderr))
+        if p.returncode != 0:
+            raise Exception('YUM: Cannot install package: %s from yum: %s' % (package, stdout + stderr))
+        return
 
 
 # Try to know in which system we are running (apt, yum, or other)
 # if cannot find a real backend, go to dummy that cannot find or install anything
 class SystemPacketMgr(object):
     def __init__(self):
-        '''
+        
         if os.name != 'nt':
-            (distname, _, _) = platform.linux_distribution()
+            (distname, distversion, _) = platform.linux_distribution()
             distname = distname.lower()
         else:
             distname = 'windows'
-
-        self.distro = 'unknown'
-        '''
+            distversion = ''
         
+        # Maybe linux_distribution did give us bull shit, so try other files like
+        # os-release
+        if distname == '':
+            if os.path.exists('/etc/os-release'):
+                with open('/etc/os-release') as f:
+                    inf = {}
+                    for line in f:
+                        k, v = line.rstrip().split("=")
+                        # .strip('"') will remove if there or else do nothing
+                        inf[k] = v.strip('"')
+                distname = inf['ID']
+                distversion = inf['VERSION_ID']
 
-        if yum:
-            self.backend = YumBackend()
-        elif apt:
+        
+        # Raw string is used by setup for display
+        self.raw_distname = distname
+        
+        self.distro = ''
+        self.distro_version = ''
+        if 'debian' in distname:
+            self.distro = 'debian'
+            self.distro_version = distversion
+        elif 'ubuntu' in distname:
+            self.distro = 'ubuntu'
+            self.distro_version = distversion
+        elif 'centos' in distname:
+            self.distro = 'centos'
+            self.distro_version = distversion
+        elif 'redhat' in distname:
+            self.distro = 'redhat'
+            self.distro_version = distversion
+        elif 'fedora' in distname:
+            self.distro = 'fedora'
+            self.distro_version = distversion
+        elif 'oracle linux' in distname:
+            self.distro = 'oracle'
+            self.distro_version = distversion
+        elif 'amzn' in distname:
+            self.distro = 'amzn'
+            self.distro_version = distversion
+        elif distname == 'windows':
+            self.distro = 'windows'
+            self.distro_version = distversion
+        else:
+            self.distro = ''
+            self.distro_version = ''
+        
+            
+
+        if self.distro in ['redhat', 'centos', 'amzn', 'oracle']:
+            if yum is not None:
+                self.backend = YumBackend()
+            else:
+                logger.error('This is a yum based linux distribution, but the python yum librairy is missing. Please install it to enable package system management.')
+                self.backend = DummyBackend()
+        elif self.distro in ['debian', 'ubuntu']:
             self.backend = AptBackend()
+        elif self.distro == 'fedora':
+            logger.error('The fedora DNF backend is not currently managed.')
+            self.backend = DummyBackend()
         else:  # oups
             self.backend = DummyBackend()
     
     
     def has_package(self, package):
         return self.backend.has_package(package)
+    
+    
+    def get_distro(self):
+        return self.distro, self.distro_version
+    
+    
+    def install_package(self, package):
+        self.backend.install_package(package)
 
 
 systepacketmgr = SystemPacketMgr()
