f4ed802771c73853ad12d101950834595b8d7355
Jean Gabès <naparuba@gmail.com>
2017-07-22 14:21:29 +0200

    Add: huge setup.py boost, that manage now several linux distributions:  * amazon linux 2016.09 & 2017.03  * centos 6 & 7  * debian 6 & 7 & 8 & 9  * alpine 3.1 (not working out of the box)  * fedora 24 & 25 (not working out of the box)  * opensuse 42.1 (not working out of the box)  * ubuntu 14.04 & 16.04 & 16.10 & 17.04 & 17.10
    Fix #10

--- setup.py
+++ setup.py
@@ -9,15 +9,10 @@ import stat
 import optparse
 import shutil
 import imp
+from cStringIO import StringIO
 from glob import glob
 
-try:
-    from setuptools import setup
-    from setuptools import find_packages
-except:
-    sys.exit("Error: missing python-setuptools library")
-
-# will fail under 1.5 python version, but if really you have such a version in
+# will fail under 2.5 python version, but if really you have such a version in
 # prod you are a morron and we can't help you
 python_version = sys.version_info
 if python_version < (2, 6):
@@ -28,6 +23,7 @@ elif python_version >= (3,):
 package_data = ['*.py']
 
 
+##################################       Utility functions for files
 # helper function to read the README file
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -50,6 +46,38 @@ def _chmodplusx(d):
         os.chmod(d, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
 
 
+##################################       Hook the ugly python setup() call that can output gcc warnings.
+# NOTE: yes, there is os.dup and file descriptor things. Deal with it (r).
+# Keep a trace of the old stdout, because setup() is just toooooooooo verbose when succeed
+stdout_orig = sys.stdout
+stderr_orig = sys.stderr
+stdout_catched = StringIO()
+stderr_redirect_path = '/tmp/stderr.kunai.tmp'
+stderr_redirect = None
+stderr_orig_bkp = None
+
+
+def hook_stdout():
+    global stderr_redirect
+    sys.stdout = stdout_catched
+    sys.stderr = stdout_catched
+    # Also hook raw stderr
+    stderr_redirect = open(stderr_redirect_path, 'w')
+    os.dup2(stderr_redirect.fileno(), 2)
+
+
+# Unhook stdout, put back fd=0
+def unhook_stdout():
+    global stderr_redirect
+    # If we have something in the file descriptor 2, reinject into stderr
+    stderr_redirect.close()
+    with open(stderr_redirect_path, 'r') as f:
+        sys.stderr.write(f.read())
+    sys.stdout = stdout_orig
+    sys.stderr = stderr_orig
+
+
+##################################       Parse arguments, especially for pip install arg catched
 parser = optparse.OptionParser("%prog [options]", version="%prog ")
 parser.add_option('--root', dest="proot", metavar="ROOT", help='Root dir to install, usefull only for packagers')
 parser.add_option('--upgrade', '--update', dest="upgrade", action='store_true', help='Only upgrade')
@@ -63,7 +91,6 @@ old_error = parser.error
 
 
 def _error(msg):
-    # print 'Parser error', msg
     pass
 
 
@@ -74,6 +101,7 @@ parser.error = old_error
 
 root = opts.proot or ''
 
+##################################       Detect install or Update
 prev_version = None
 prev_path = ''
 # We try to see if we are in a full install or an update process
@@ -96,7 +124,7 @@ try:
     prev_path = os.path.dirname(kunai_test_import.__file__)
     del kunai_test_import
 except ImportError, exp:  # great, first install so
-    print "No previous kunai installation found: %s" % exp
+    pass
 
 # Now look at loading the local kunai lib for version and banner
 my_dir = os.path.dirname(os.path.abspath(__file__))
@@ -105,11 +133,12 @@ from kunai.info import VERSION, BANNER
 from kunai.log import cprint
 from kunai.systempacketmanager import systepacketmgr
 
-
+##################################       Only root as it's a global system tool.
 if os.getuid() != 0:
     cprint('Setup must be launched as root.', color='red')
     sys.exit(2)
 
+##################################       Start to print to the user
 cprint(BANNER, color='green')
 
 what = 'Installing' if not is_update else 'Updating'
@@ -120,7 +149,7 @@ cprint('%s' % VERSION, color='magenta', end='')
 cprint('     %s' % ('*' * 20))
 
 if is_update:
-    cprint('Previous Kunai lib detected on this system (location:', end='')
+    cprint('  Previous Kunai lib detected on this system (location:', end='')
     cprint(prev_path, color='blue', end='')
     cprint(')(version:', end='')
     cprint('%s' % prev_version, color='blue', end='')
@@ -137,7 +166,6 @@ if '--update' in args or opts.upgrade or '--upgrade' in args:
         sys.argv.remove('--update')
     if '--upgrade' in args:
         sys.argv.remove('--upgrade')
-    print "Kunai lib update only"
     is_update = True
 
 is_install = False
@@ -166,6 +194,7 @@ for idx in to_del:
 if '-v' not in sys.argv and '--quiet' not in sys.argv and '-q' not in sys.argv:
     sys.argv.insert(1, '--quiet')
 
+##################################       Prepare the list of files that will be installed
 # compute scripts
 scripts = [s for s in glob('bin/kunai*') if not s.endswith('.py')]
 
@@ -233,14 +262,12 @@ if not is_update:
         if len(files) == 0:
             data_files.append((os.path.join(default_paths['var'], re.sub(r"^(data\/|data$)", "", path)), []))
         for name in files:
-            data_files.append((os.path.join(default_paths['var'], re.sub(r"^(data\/|data$)", "", path)),
-                               [os.path.join(path, name)]))
+            data_files.append((os.path.join(default_paths['var'], re.sub(r"^(data\/|data$)", "", path)), [os.path.join(path, name)]))
 
 # Libexec is always installed
 for path, subdirs, files in os.walk('libexec'):
     for name in files:
-        data_files.append((os.path.join(default_paths['libexec'], re.sub(r"^(libexec\/|libexec$)", "", path)),
-                           [os.path.join(path, name)]))
+        data_files.append((os.path.join(default_paths['libexec'], re.sub(r"^(libexec\/|libexec$)", "", path)), [os.path.join(path, name)]))
 
 data_files.append((default_paths['run'], []))
 data_files.append((default_paths['log'], []))
@@ -256,68 +283,203 @@ for o in not_allowed_options:
     if o in sys.argv:
         sys.argv.remove(o)
 
+##################################       Look at prerequites, and if possible fix them with the system package instead of pip
 cprint('# %-30s  (1/3)' % 'Checking prerequites')
-mod_need = ['requests', 'cherrypy', 'leveldb', 'jinja2', 'rsa', 'pyasn1', 'pycurl', 'crypto']
-# leveldb and setproctitle are not available on windows
-if os.name != 'nt':
-    mod_need.append('leveldb')
-    mod_need.append('setproctitle')
+# Maybe we won't be able to setup with packages, if so, switch to pip :(
+install_from_pip = []
 
-for m in mod_need:
+mod_need = {
+    'requests': {
+        'packages': {
+            'debian': 'python-requests', 'ubuntu': 'python-requests',
+            'amzn'  : 'python27-requests', 'centos': 'python-requests', 'redhat': 'python-requests', 'oracle': 'python-requests', 'fedora': 'python-requests',
+        }
+    },
+    'cherrypy': {  # note: centos: first epel to enable cherrypy get from packages
+        'packages'    : {
+            'debian': 'python-cherrypy3', 'ubuntu': 'python-cherrypy3',
+            'amzn'  : 'python-cherrypy3', 'centos': ['epel-release', 'python-cherrypy'], 'redhat': 'python-cherrypy', 'oracle': 'python-cherrypy', 'fedora': 'python-cherrypy',
+        },
+        'failback_pip': 'cherrypy==3.2.4',
+    },
+    'jinja2'  : {
+        'packages': {
+            'debian': 'python-jinja2', 'ubuntu': 'python-jinja2',
+            'amzn'  : 'python-jinja2', 'centos': 'python-jinja2', 'redhat': 'python-jinja2', 'oracle': 'python-jinja2', 'fedora': 'python-jinja2',
+        }
+    },
+    # 'rsa'     : {
+    #    'packages': {}
+    # },
+    # 'pyasn1'  : {
+    #    'packages': {}
+    # },
+    'pycurl'  : {
+        'packages': {
+            'debian': 'python-pycurl', 'ubuntu': 'python-pycurl',
+            'amzn'  : 'python-pycurl', 'centos': 'python-pycurl', 'redhat': 'python-pycurl', 'oracle': 'python-pycurl', 'fedora': 'python-pycurl',
+        }
+    },
+    'Crypto'  : {
+        'packages': {
+            'debian': 'python-crypto', 'ubuntu': 'python-crypto',
+            'amzn'  : 'python-crypto', 'centos': 'python-crypto', 'redhat': 'python-crypto', 'oracle': 'python-crypto', 'fedora': 'python-crypto',
+        }
+    },
+}
+# leveldb is not available on windows
+if os.name != 'nt':
+    mod_need['leveldb'] = {
+        'packages'    : {
+            'debian': 'python-leveldb', 'ubuntu': 'python-leveldb',
+            'amzn'  : 'python-leveldb', 'centos': 'python-leveldb', 'redhat': 'python-leveldb', 'oracle': 'python-leveldb', 'fedora': 'python-leveldb',
+        },
+        'pip_packages': {
+            'debian': ['build-essential', 'python-dev'], 'ubuntu': ['build-essential', 'python-dev'],
+            # NOTE: amazon: no python-devel/python-setuptools, only versionsed packages are available
+            'amzn'  : ['gcc', 'gcc-c++', 'python27-devel', 'libyaml-devel', 'python27-setuptools'], 'centos': ['gcc', 'gcc-c++', 'python-devel', 'libyaml-devel'], 'redhat': ['gcc', 'gcc-c++', 'python-devel', 'libyaml-devel'],
+            'oracle': ['gcc', 'gcc-c++', 'python-devel', 'libyaml-devel'], 'fedora': ['gcc', 'gcc-c++', 'python-devel', 'libyaml-devel'],
+        },
+    }
+
+# We will have to look in which distro we are
+system_distro, system_distroversion = systepacketmgr.get_distro()
+if system_distro != '':
+    cprint(' * Your system ', end='')
+    cprint('%s (version %s) ' % (system_distro, system_distroversion), color='magenta', end='')
+    cprint('is managed by this installer and will be able to use system package manager to install dependencies.')
+else:
+    cprint(" * NOTICE: your system (%s) is not a tested system, it won't use the package system to install dependencies and will use the python pip dependency system instead (internet connection is need)." % systepacketmgr.raw_distname)
+
+for (m, d) in mod_need.iteritems():
+    cprint(' * checking dependency for ', end='')
+    cprint('%-20s' % m, color='blue', end='')
+    cprint(' : ', end='')
+    sys.stdout.flush()
     try:
         __import__(m)
+        cprint('OK', color='green')
     except ImportError:
-        cprint('Warning: cannot import module %s. You must install if before launch the kunai deamon' % m, color='yellow')
+        cprint('MISSING', color='cyan')
+        packages = d['packages']
+        to_install = packages.get(system_distro, '')
+        pip_failback = d.get('failback_pip', m)
+        if not to_install:
+            cprint('   - Cannot find valid packages from system packages on this distribution for the module %s, will be installed by the python pip system instead (need an internet connection)' % m, color='yellow')
+            install_from_pip.append(pip_failback)
+        else:
+            if isinstance(to_install, basestring):
+                to_install = [to_install]
+            for pkg in to_install:
+                cprint('   - Trying to install the package ', color='grey', end='')
+                cprint('%-20s' % pkg, color='blue', end='')
+                cprint(' from system packages  : ', color='grey', end='')
+                sys.stdout.flush()
+                try:
+                    systepacketmgr.install_package(pkg)
+                    cprint('OK', color='green')
+                    # __import__(m)
+                except Exception, exp:
+                    cprint('(missing in package)', color='cyan')
+                    cprint('   - cannot install the package from the system. Switching to an installation based on the python pip system (need an internet connection)', color='grey')
+                    _prefix = '      | '
+                    cprint('\n'.join(['%s%s' % (_prefix, s) for s in str(exp).splitlines()]), color='grey')
+                    
+                    install_from_pip.append(pip_failback)
+                    all_pip_packages = d.get('pip_packages', {})
+                    pip_packages = all_pip_packages.get(system_distro, [])
+                    for pip_pkg in pip_packages:
+                        try:
+                            cprint('   - Install from system package the python lib dependency: ', color='grey', end='')
+                            cprint(pip_pkg)
+                            systepacketmgr.install_package(pip_pkg)
+                        except Exception, exp:
+                            cprint('    - WARNING: cannot import python lib dependency: %s : %s' % (pip_pkg, exp))
+                            
+                            # Remove duplicate from pip install
+install_from_pip = set(install_from_pip)
 
+# Try to import setup tools, and if not, switch to
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    try:
+        cprint(' * You are missing the python setuptools, trying to install it with system package:', end='')
+        sys.stdout.flush()
+        systepacketmgr.install_package('python-setuptools')
+        cprint(' OK', color='green')
+        from setuptools import setup, find_packages
+    except Exception, exp:
+        cprint('Cannot install python setuptools from system (%s). Cannot continue the installation. Please install python-setuptools before re-run the installation.' % exp, color='red')
+        sys.exit(2)
 
+##################################       Go install the python part
 cprint('\n\n# %-30s  (2/3)' % 'Python lib installation')
-cprint('%s kunai python lib in progress...' % what, end='')
+cprint('  * %s kunai python lib in progress...' % what, end='')
 sys.stdout.flush()
 
+hook_stdout()
 
-setup(
-    name="kunai",
-    version=VERSION,
-    packages=find_packages(),
-    package_data={'': package_data},
-    description="Kunai is a service discovery tool",
-    long_description=read('README.md'),
-    author="Gabes Jean",
-    author_email="naparuba@gmail.com",
-    license="MIT",
-    url="http://kunai.io",
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2 :: Only',
-        'Topic :: System :: Monitoring',
-        'Topic :: System :: Networking :: Monitoring',
-        'Topic :: System :: Distributed Computing',
-    ],
-    data_files=data_files,
-)
+try:
+    setup(
+        name="kunai",
+        version=VERSION,
+        packages=find_packages(),
+        package_data={'': package_data},
+        description="Kunai is a service discovery tool",
+        long_description=read('README.md'),
+        author="Gabes Jean",
+        author_email="naparuba@gmail.com",
+        license="MIT",
+        url="http://kunai.io",
+        zip_safe=False,
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2 :: Only',
+            'Topic :: System :: Monitoring',
+            'Topic :: System :: Networking :: Monitoring',
+            'Topic :: System :: Distributed Computing',
+        ],
+        install_requires=[install_from_pip],
+        data_files=data_files,
+    )
+except Exception, exp:
+    unhook_stdout()
+    cprint('ERROR: fail to setup kunai: (%s)' % exp, color='red')
+    cprint(stdout_catched.getvalue())
+    sys.exit(2)
+
+# We did finish the setup, and we did succeed, so we can put the result into a log, we don't fucking care about
+# printing it to everyone unless we want to fear them
+unhook_stdout()
 
 cprint('  OK', color='green')
+
+installation_log = '/tmp/kunai.setup.log'
+with open(installation_log, 'w') as f:
+    f.write(stdout_catched.getvalue())
+    cprint('   - Raw python setup lib (and possible depndencies) installation log at: %s' % installation_log, color='grey')
+
+##################################       Install init.d script, the daemon script and bash completion part
+cprint('\n\n# %-30s  (3/3)' % 'Utility script installation')
 
 
 # Just a print with aligned test over : OK
 def __print_sub_install_part(p):
-    cprint('  * %-30s :' % p, end='')
+    cprint('   - %-30s :' % p, color='grey', end='')
     cprint(' OK', color='green')
 
 
-cprint('\n\n# %-30s  (3/3)' % 'Utility script installation')
-
 # if root is set, it's for package, so NO chown
 if not root and is_install:
-    cprint('Installing utility scripts (init.d, daemon, bash completion, etc)')
+    cprint(' * Installing utility scripts (init.d, daemon, bash completion, etc)')
     # Also change the rights of the kunai- scripts
     for s in scripts:
         bs = os.path.basename(s)
