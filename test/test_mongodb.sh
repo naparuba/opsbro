1ecb4959ebd96913b430f776bff017c49a7755e9
Jean Gabès <naparuba@gmail.com>
2017-07-23 22:30:20 +0200

    Add: test for mongodb, based on centos 7 + mongodb & check collector TODO: embeeded pymongo

---
+++ test/test_mongodb.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+
+sleep 15
+
+RES=$(kunai evaluator eval "{{collector.mongodb.available}}==True" | tail -n 1)
+
+if [ $RES != "True" ]; then
+    echo "Fail: mongodb collectors is not running $RES"
+    kunai collectors show mongodb
+    exit 2
+fi
+
+kunai collectors show mongodb
+echo "Mongodb is OK"
+
+
