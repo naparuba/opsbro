e07ae094985aaea5b2518de87ee9a97a21bf90ae
Jean Gabès <naparuba@gmail.com>
2017-07-29 10:45:26 +0200

    Add: first test about apis

---
+++ test/test_apis.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+/etc/init.d/kunai start
+
+sleep 30
+
+echo "Starting to test APIS"
+
+
+echo "********** /packs *********"
+RES=$(curl -s http://localhost:6768/packs | jq '.[0].name')
+
+if [ $? != 0 ] || [ $RES == "null" ]; then
+    echo "ERROR: the /packs do not returns good results: $RES"
+    exit 2
+fi
+echo "OK:  /packs did return $RES"
+
+echo "All APIS are OK"
