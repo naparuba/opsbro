cfcaeb4abb9fc965ee5f83cc5fef2370a31716a8
Jean Gabès <naparuba@gmail.com>
2017-09-01 10:26:16 +0200

    Enh: TRAVIS should be faster, remove useless dependencies for pure docker launch, less CPU usage for travis.

--- test/test_suite.sh
+++ test/test_suite.sh
@@ -3,7 +3,14 @@
 
 # Travis: only need to run the installation once, it it not link to a specific python version. They don't need to use CPU for nothing ;)
 if [ "$TEST_SUITE" == "PYTHON" ]; then
+   echo "Installing dependencies for TESTING"
+   pip install -r dependencies
+   pip install coveralls
+   pip install nose-cov
+   pip install unittest2
+
    echo "Test launch for Python"
+   if [[ $TRAVIS_PYTHON_VERSION == '2.6' ]]; then pip install ordereddict; fi
    nosetests -xv --processes=1 --process-timeout=300 --process-restartworker --with-cov --cov=opsbro --exe
    exit $?
 fi
