790ff78a531ae9e11bbb156bb63b175f8b1271f6
Jean Gabès <naparuba@gmail.com>
2017-09-29 20:52:54 +0200

    Enh: shinken enterprise module now prepare a payload for future send

--- data/global-configuration/packs/shinken-enterprise/module/module.py
+++ data/global-configuration/packs/shinken-enterprise/module/module.py
@@ -5,6 +5,7 @@ from opsbro.parameters import StringParameter, BoolParameter
 from opsbro.gossip import gossiper
 from opsbro.collectormanager import collectormgr
 
+
 class ShinkenEnterpriseModule(ConnectorModule):
     implement = 'shinken-enterprise'
     
@@ -22,20 +23,87 @@ class ShinkenEnterpriseModule(ConnectorModule):
         with gossiper.groups_lock:
             groups = gossiper.groups[:]
         self.logger.info('Pushing back ours groups and discovery informations to Shinken Enterprise')
-
+        
         collectors_data = {}
         for (ccls, e) in collectormgr.collectors.iteritems():
             cname, c = collectormgr.get_collector_json_extract(e)
             collectors_data[cname] = c
-
+        
         # TODO: push to shinken now :)
         f = open('/tmp/shinken-local-discovery.json', 'w')
         
-        data = {'groups': groups,
-                'collectors': collectors_data,
-                }
-        
+        data = {
+            'uuid'      : gossiper.uuid,
+            'templates' : groups,
+            'collectors': collectors_data,
+        }
         f.write(json.dumps(data, indent=4))
-        
         f.close()
         
+        payload = {
+            'uuid'     : gossiper.uuid,
+            'templates': groups,
+            'datas'    : {}
+        }
+        
+        datas = payload['datas']
+        
+        # System info
+        system_results = collectors_data.get('system', {}).get('results', {})
+        
+        hostname = system_results.get('hostname', '')
+        payload['hostname'] = hostname
+        
+        fqdn = system_results.get('fqdn', '')
+        if fqdn:
+            datas['FQDN'] = fqdn
+        
+        publicip = system_results.get('publicip', '')
+        if publicip:
+            datas['PUBLIC_IP'] = publicip
+        
+        # which address to use in fact?
+        # how to choose:   fqdn > public_ip > hostname
+        if fqdn:
+            payload['address'] = fqdn
+        elif publicip:
+            payload['address'] = publicip
+        else:
+            payload['address'] = hostname
+        
+        # Timezone
+        timezone = collectors_data.get('timezone', {}).get('results', {}).get('timezone', '')
+        if timezone:
+            datas['TIMEZONE'] = timezone
+        
+        cpucount = system_results.get('cpucount', '')
+        if cpucount:
+            datas['CPU_COUNT'] = cpucount
+        
+        linux_distribution = system_results.get('os', {}).get('linux', {}).get('distribution', '')
+        if linux_distribution:
+            datas['LINUX_DISTRIBUTION'] = linux_distribution
+        
+        # Memory
+        physical_memory = collectors_data.get('timezone', {}).get('results', {}).get('phys_total', '')
+        if physical_memory:
+            datas['PHYSICAL_MEMORY'] = physical_memory
+        
+        # Network
+        network_interfaces = ','.join(collectors_data.get('interfaces', {}).get('results', {}).keys())
+        if network_interfaces:
+            datas['NETWORK_INTERFACES'] = network_interfaces
+        
+        # Geoloc (lat and long)
+        geoloc = collectors_data.get('geoloc', {}).get('results', {}).get('loc', '')
+        if geoloc and geoloc.count(',') == 1:
+            lat, long = geoloc.split(',', 1)
+            datas['LAT'] = lat
+            datas['LONG'] = long
+        
+        # disks
+        volumes = ','.join(collectors_data.get('diskusage', {}).get('results', {}).keys())
+        if volumes:
+            datas['VOLUMES'] = volumes
+        
+        print 'Payload to send', payload
