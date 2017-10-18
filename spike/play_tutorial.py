9b6f0238b950519bac7b1c41665a006ca4d7c598
Jean Gabès <naparuba@gmail.com>
2017-10-18 21:37:50 +0200

    Add: buzz play_tutorial command

--- spike/play_tutorial.py
+++ spike/play_tutorial.py
@@ -1,7 +1,12 @@
+
 import json
+import os
 import time
 from opsbro.log import cprint
-pth = '/tmp/share/installation.json'
+from opsbro.info import BANNER, TITLE_COLOR
+import sys
+
+pth = os.path.join(os.path.dirname(__file__), 'linux-dashboard.json')
 
 f = open(pth, 'r')
 buf = f.read()
@@ -9,16 +14,32 @@ f.close()
 
 tutorial = json.loads(buf, encoding='utf8')
 
-print "Tutorial", tutorial.keys()
 
+cprint('Playing %s dashboard sample (preview)' % TITLE_COLOR)
+time.sleep(1)
 
-print "Will play %s (total time %.2fsec)" % (tutorial['title'], tutorial['duration'])
+clean = '\033c'
+first_clean_read = False
 
 stdout_entries = tutorial['stdout']
 for e in stdout_entries:
     wait_time = e[0]
     line = e[1]
+    if clean in line:
+        if first_clean_read:
+            time.sleep(1)
+        first_clean_read = True
+        wait_time = 0.01
     cprint(line, end='')
+    sys.stdout.flush()
     time.sleep(wait_time)
     
-print "FINISH"
+cprint('\n\n\n')
+
+cprint(BANNER)
+cprint('You did enjoy a preview of OpsBro dashboard, here for the linux system. More to see at ', end='')
+cprint('https://github.com/naparuba/opsbro/', color='magenta')
+cprint('And if you want a freaking good and powerful monitoring tool for large corps, look at ', end='')
+cprint('Shinken framework & Shinken Enterprise', color='magenta', end='')
+cprint(' the main projects of Shinken Solutions team at ', end='')
+cprint('https://www.shinken-enterprise.com', color='magenta')
