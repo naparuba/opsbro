221c33c400eb110623f0ace4d8431996a159ac21
Jean Gabès <naparuba@gmail.com>
2017-10-20 10:22:14 +0200

    Enh: switching the demo to a curl based system on static version so can still dev in github)

--- spike/play-linux-dashboard.sh
+++ spike/play-linux-dashboard.sh
@@ -41,6 +41,7 @@ pythonver
 
 if [ $? == 0 ];then
   echo "Getting the OpsBro source from github"
+  rm -fr /tmp/opsbro.tar.gz /tmp/opsbro
   curl -s http://linux.dashboard.static.opsbro.io/opsbro.tar.gz > /tmp/opsbro.tar.gz
   cd /tmp
   tar xfz opsbro.tar.gz
@@ -54,6 +55,10 @@ echo "All is OK, launching the dashboard"
 sleep 2
 
 LANG=en_US.UTF8 $PYTHON -u bin/opsbro dashboards show linux
+if [ $? != 0 ]; then
+   echo "Oups, something get wrong on the agent start, please report it at the github issues (https://github.com/naparuba/opsbro/issues)"
+   exit 2
+fi
 
 printf "Demo is finish, OpsBro is able of far more than just a dashboard, more to come in the documentation very soon (⌐■_■)\n"
 sleep 2
