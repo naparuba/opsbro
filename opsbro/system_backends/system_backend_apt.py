17245a059540d9deb925561c5e5203b001455ee7
Jean Gabès <naparuba@gmail.com>
2018-02-28 16:35:12 +0100

    Enh: move apt & yum system bckend to their own files

---
+++ opsbro/system_backends/system_backend_apt.py
@@ -0,0 +1,103 @@
+import time
+import os
+import subprocess
+
+from opsbro.log import LoggerFactory
+
+# Global logger for this part
+logger = LoggerFactory.create_logger('system-packages')
+
+
+class AptBackend(object):
+    def __init__(self):
+        self.deb_cache = None
+        self.deb_cache_update_time = 0
+        self.DEB_CACHE_MAX_AGE = 60  # if we cannot look at dpkg data age, allow a max cache of 60s to get a new apt update from disk
+        self.DPKG_CACHE_PATH = '/var/cache/apt/pkgcache.bin'
+        self.dpkg_cache_last_modification_epoch = 0.0
+        # query cache, invalidate as soon as the apt cache is gone too
+        self.has_cache = {}
+        
+        try:
+            import apt
+        except ImportError:
+            apt = None
+        self.apt = apt
+    
+    
+    # Maybe the apt module is not present, if so, fix it
+    def _assert_apt(self):
+        if self.apt is None:
+            try:
+                self.install_package('python-apt')
+                import apt
+                self.apt = apt
+            except Exception, exp:
+                logger.error('Cannot install APT python lib: %s' % exp)
+                return
+    
+    
+    def has_package(self, package):
+        self._assert_apt()
+        if self.apt is None:
+            return False
+        t0 = time.time()
+        if not self.deb_cache:
+            self.deb_cache = self.apt.Cache()
+            self.deb_cache_update_time = int(time.time())
+            self.has_cache = {}
+        else:  # ok already existing, look if we should update it
+            # because if there was a package installed, it's no more in cache
+            need_reload = False
+            if os.path.exists(self.DPKG_CACHE_PATH):
+                last_change = os.stat(self.DPKG_CACHE_PATH).st_mtime
+                if last_change != self.dpkg_cache_last_modification_epoch:
+                    need_reload = True
+                    self.dpkg_cache_last_modification_epoch = last_change
+            else:  # ok we cannot look at the dpkg file age, must limit by time
+                # the cache is just a memory view, so if too old, need to udpate it
+                if self.deb_cache_update_time < time.time() - self.DEB_CACHE_MAX_AGE:
+                    need_reload = True
+            if need_reload:
+                self.deb_cache.open(None)
+                self.deb_cache_update_time = int(time.time())
+                self.has_cache = {}
+        b = self.has_cache.get(package, None)
+        if b is None:
+            b = (package in self.deb_cache and self.deb_cache[package].is_installed)
+            logger.debug('APT: cache miss. time to query apt for package %s: %.3f' % (package, time.time() - t0))
+        return b
+    
+    
+    # apt-get -q --yes --no-install-recommends install XXXXX
+    @staticmethod
+    def install_package(package):
+        logger.debug('APT :: installing package: %s' % package)
+        p = subprocess.Popen(['apt-get', 'update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('APT (apt-get update):: stdout/stderr: %s/%s' % (stdout, stderr))
+        if p.returncode != 0:
+            raise Exception('APT: apt-get update did not succeed (%s), exiting from package installation (%s)' % (stdout + stderr, package))
+        p = subprocess.Popen(['apt-get', '-q', '--yes', '--no-install-recommends', 'install', r'%s' % package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('APT (apt-get install) (%s):: stdout/stderr: %s/%s' % (package, stdout, stderr))
+        if p.returncode != 0:
+            raise Exception('APT: apt-get install did not succeed (%s), exiting from package installation (%s)' % (stdout + stderr, package))
+        return
+    
+    
+    # apt-get -q --yes --no-install-recommends install XXXXX
+    @staticmethod
+    def update_package(package):
+        logger.debug('APT :: updating package: %s' % package)
+        p = subprocess.Popen(['apt-get', 'update'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('APT (apt-get update):: stdout/stderr: %s/%s' % (stdout, stderr))
+        if p.returncode != 0:
+            raise Exception('APT: apt-get update did not succeed (%s), exiting from package installation (%s)' % (stdout + stderr, package))
+        p = subprocess.Popen(['apt-get', '-q', '--yes', '--no-install-recommends', 'update', r'%s' % package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('APT (apt-get update) (%s):: stdout/stderr: %s/%s' % (package, stdout, stderr))
+        if p.returncode != 0:
+            raise Exception('APT: apt-get update did not succeed (%s), exiting from package installation (%s)' % (stdout + stderr, package))
+        return
---
+++ opsbro/system_backends/system_backend_yum.py
@@ -0,0 +1,57 @@
+import time
+import os
+import platform
+import subprocess
+import threading
+
+from opsbro.log import LoggerFactory
+
+# Global logger for this part
+logger = LoggerFactory.create_logger('system-packages')
+
+
+class YumBackend(object):
+    def __init__(self):
+        self.yumbase = None
+        self.yumbase_lock = threading.RLock()
+        
+        try:
+            import yum
+        except ImportError:
+            yum = None
+        self.yum = yum
+    
+    
+    def has_package(self, package):
+        # Yum conf seem to be global and so cannot set it in 2 threads at the same time
+        with self.yumbase_lock:
+            if not self.yumbase:
+                self.yumbase = self.yum.YumBase()
+                self.yumbase.conf.cache = 1
+        return package in (pkg.name for pkg in self.yumbase.rpmdb.returnPackages())
+    
+    
+    # yum  --nogpgcheck  -y  --rpmverbosity=error  --errorlevel=1  --color=auto  install  XXXXX
+    @staticmethod
+    def install_package(package):
+        logger.debug('YUM :: installing package: %s' % package)
+        p = subprocess.Popen(['yum', '--nogpgcheck', '-y', '--rpmverbosity=error', '--errorlevel=1', '--color=auto', 'install', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('YUM (%s):: stdout: %s' % (package, stdout))
+        logger.debug('YUM (%s):: stderr: %s' % (package, stderr))
+        if p.returncode != 0:
+            raise Exception('YUM: Cannot install package: %s from yum: %s' % (package, stdout + stderr))
+        return
+    
+    
+    # yum  --nogpgcheck  -y  --rpmverbosity=error  --errorlevel=1  --color=auto  install  XXXXX
+    @staticmethod
+    def update_package(package):
+        logger.debug('YUM :: update package: %s' % package)
+        p = subprocess.Popen(['yum', '--nogpgcheck', '-y', '--rpmverbosity=error', '--errorlevel=1', '--color=auto', 'update', package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        logger.debug('YUM (%s):: stdout: %s' % (package, stdout))
+        logger.debug('YUM (%s):: stderr: %s' % (package, stderr))
+        if p.returncode != 0:
+            raise Exception('YUM: Cannot update package: %s from yum: %s' % (package, stdout + stderr))
+        return
