6689bd08af49085e7524de67f50ab1ef58c02f02
Jean Gabès <naparuba@gmail.com>
2018-04-04 11:59:44 +0200

    Enh: TEST now the DEMO1 scenario is using event to synchronize between docker containers (some can be very slow to start)

--- test/test_demo1_http.sh
+++ test/test_demo1_http.sh
@@ -141,7 +141,7 @@ if [ $CASE == "NODE-HAPROXY" ]; then
     echo "============================== [`date`] $CASE waiting for HTTP node to be ready ================================="
 
     ps axjf
-    netstat -tulpn | grep 80
+
 
     # Wait until the http-1 and http-2 are ready to be queried
     wait_step_event_node "HTTP-READY" "NODE-HTTP-1"
@@ -186,7 +186,6 @@ if [ $CASE == "NODE-HAPROXY" ]; then
     cat /var/log/messages
 
     ps axjf
-    netstat -tulpn | grep 80
 
     echo "HAPROXY: look if local proxying is valid  `date`"
     OUT=$(curl -s http://localhost)
@@ -209,7 +208,6 @@ if [ $CASE == "NODE-HAPROXY" ]; then
     echo "HAPROXY: `date` NODE-HTTP-2 is present"
 
     ps axjf
-    netstat -tulpn | grep 80
 
     grep 'NODE-HTTP-1' /etc/haproxy/haproxy.cfg > /dev/null
     if [ $? != 0 ];then
@@ -231,7 +229,6 @@ if [ $CASE == "NODE-HAPROXY" ]; then
     fi
 
     ps axjf
-    netstat -tulpn | grep 80
 
     NB_UP=$(echo "$STATS" | grep 'back_http' | grep -v BACKEND | grep UP | wc -l)
     if [ "$NB" != "2" ];then
