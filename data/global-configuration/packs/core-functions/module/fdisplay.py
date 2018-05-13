10e0c44acfbfa36237696d9f331d804572330235
Jean Gabès <naparuba@gmail.com>
2018-05-13 14:53:01 +0200

    Enh: MONITORING memory checks now display a horizontal bar

--- data/global-configuration/packs/core-functions/module/fdisplay.py
+++ data/global-configuration/packs/core-functions/module/fdisplay.py
@@ -26,3 +26,22 @@ def get_donut(value):
     from opsbro.cli_display import DonutPrinter
     donut_string = DonutPrinter().get_donut(value)
     return donut_string
+
+@export_evaluater_function(function_group=FUNCTION_GROUP)
+def get_horizontal_bar(value, width=80):
+    u"""**get_horizontal_bar(value, width=80)** -> return a string a horizontal chart
+
+ * value: float between 0 and 1
+
+<code>
+    Example:
+        get_horizontal_bar(0.53)
+    Returns:
+█████████████████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+</code>
+    """
+    if value > 1 and value <= 100:
+        value = value / 100
+    from opsbro.cli_display import HBarPrinter
+    hbar_string = HBarPrinter().get_hbar(value, width=width)
+    return hbar_string
