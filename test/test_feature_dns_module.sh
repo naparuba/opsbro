577c36ebbb84453a7ba0766621a12fc4a1aa74ed
Jean Gabès <naparuba@gmail.com>
2018-07-13 15:06:42 +0200

    Enh: PACK DNS: automatically setup dnsmasq Enh: TEST dns module, test the dnsmasq setup too

--- test/test_feature_dns_module.sh
+++ test/test_feature_dns_module.sh
@@ -9,11 +9,18 @@ echo "Starting to test DNS module"
 # Enable DNS module
 opsbro agent parameters add groups dns-listener
 
-# Wait for detection and co
-# TODO: why so long?
-sleep 15
+
+opsbro compliance wait-compliant "Install local dns relay" --timeout=60
+if [ $? != 0 ];then
+   echo "ERROR: the local dns cannot be installed"
+   opsbro generators state
+   opsbro generators history
+   cat /var/log/opsbro/generator.log
+   cat /var/log/opsbro/crash.log 2>/dev/null
+fi
 
 
+# Now look we are ready
 # Look which addr we dhould match
 ADDR=$(opsbro agent print public-addr)
 
@@ -24,7 +31,7 @@ if [ "X$ADDR" == "X" ];then
 fi
 
 # linux is detected, so should return
-echo "Looking for my own entry $ADDR"
+echo "Looking for my own entry $ADDR directly on daemon"
 OUT=$(dig -p 6766  @127.0.0.1 linux.group.local.opsbro)
 printf "$OUT" | grep "$ADDR"
 if [ $? != 0 ]; then
@@ -38,5 +45,24 @@ if [ $? != 0 ]; then
     exit 2
 fi
 
+
+# Also the local dsnmasq
+echo "Looking for my own entry $ADDR but in the system DNS"
+OUT=$(dig linux.group.local.opsbro)
+printf "$OUT" | grep "$ADDR"
+if [ $? != 0 ]; then
+    echo "The DNS module do not seems to result"
+    printf "$OUT"
+    opsbro agent info
+    opsbro compliance history
+    cat /var/log/opsbro/module.dns.log
+    hostname
+    ping -c 1 `hostname`
+    cat /etc/resolv.conf
+    cat /etc/dnsmasq.d/opsbro.conf
+    ps axjf
+    netstat -laputen | grep LISTEN
+    exit 2
+fi
 
 echo "opsbro DNS module is OK"
