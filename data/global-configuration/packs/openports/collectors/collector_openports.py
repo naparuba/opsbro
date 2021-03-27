bd74c0b819d99350ca1dbd4cb06d86967cae37e6
Jean Gabès <naparuba@gmail.com>
2021-03-27 22:02:38 +0100

    Fix: as we have the psutil lib, we can use it for open connexions

--- data/global-configuration/packs/openports/collectors/collector_openports.py
+++ data/global-configuration/packs/openports/collectors/collector_openports.py
@@ -1,76 +1,34 @@
-from __future__ import print_function
-import sys
-import traceback
-
 from opsbro.collector import Collector
 
 
 class OpenPorts(Collector):
     def launch(self):
-        logger = self.logger
-        # logger.debug('get_open_ports: start')
-        
         open_port_details = {'tcp': [], 'udp': []}
         open_ports = set()  # just the bunch of int
         
-        if sys.platform != 'linux2':
-            logger.debug('get_open_ports: unsupported platform')
-            return False
-        
-        try:
-            _cmd = 'netstat -tuln'
+        if not hasattr(self, 'psutil_lib'):
             try:
-                netstat = self.execute_shell(_cmd, if_fail_set_error=False, if_fail_not_eligible=True, not_eligible_text='Cannot call netstat. Please install it.')
-            except Exception as exp:
-                self.set_not_eligible('get_open_ports: cannot start netstat: %s. Please install it.' % exp)
+                import psutil
+                self.psutil_lib = psutil
+            except ImportError as exp:
+                self.set_not_eligible('get_open_ports: cannot import psutil lib %s. Please install it.' % exp)
                 return False
-            if netstat is False:
-                return False
-            
-            for line in netstat.splitlines():
-                line = line.strip()
-                
-                # Will be something like
-                # tcp        0      0 0.0.0.0:27017           0.0.0.0:*               LISTEN
-                if not line.startswith('tcp') and not line.startswith('udp'):
-                    # Not a good line, skip it
-                    continue
-                
-                elts = [e for e in line.split(' ') if e]
-                
-                if len(elts) != 6:  # bad line
-                    continue
-                
-                if elts[5] != 'LISTEN':  # not a listen port
-                    continue
-                
-                open_port = {}
-                open_port['proto'] = elts[0]
-                open_port['source'] = elts[3]
-                open_port['dest'] = elts[4]
-                open_port['state'] = elts[5]
-                
-                # no : mean no port
-                if ':' not in open_port['source']:
-                    continue
-                
-                _port = int(open_port['source'].split(':')[-1])
-                open_ports.add(_port)
-                
-                if open_port['proto'].startswith('tcp'):
-                    open_port_details['tcp'].append(open_port)
-                elif open_port['proto'].startswith('udp'):
-                    open_port_details['udp'].append(open_port)
-                else:
-                    self.set_error("Unknown protocol?? %s" % open_port['proto'])
-                    return False
         
-        except Exception:
-            self.set_error('get_open_ports: exception = %s' % traceback.format_exc())
-            return False
+        listening_connexions = [con for con in self.psutil_lib.net_connections() if con.status == 'LISTEN']
+        for con in listening_connexions:
+            # UDP: sconn(fd=7, family=10, type=1, laddr=('::', 53)
+            # TCP: sconn(fd=26, family=2, type=1, laddr=('0.0.0.0', 6769), raddr=(), status='LISTEN', pid=26)
+            if len(con.laddr) != 2:  # bad format
+                continue
+            laddr = con.laddr[0]
+            lport = con.laddr[1]
+            open_ports.add(lport)
+            
+            if con.family == 10:  # UDP
+                open_port_details['udp'].append(laddr)
+            else:
+                open_port_details['tcp'].append(laddr)
         
         open_ports = list(open_ports)
         open_ports.sort()
-        
-        # logger.debug('get_open_ports: completed, returning')
         return {'ports': open_ports, 'details': open_port_details}
